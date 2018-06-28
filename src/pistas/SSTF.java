b2bed048756c5620eeb16008853a54620345c65a
ivanpbf <ivan27rlb@gmail.com>
2018-06-27 23:40:17 -0430

    Creacion del metodo SSTF
    creo que esta bien?

--- src/pistas/SSTF.java
+++ src/pistas/SSTF.java
@@ -6,6 +6,8 @@
 package pistas;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,6 +17,72 @@ public class SSTF extends Metodos{
     
     public SSTF(List PNS, int PI) {
         super(PNS, PI);
+    }
+//pi pista en la que inicia el brazo
+    @Override
+    public void run() {
+        //tiempo en que inicia la simulacion
+        double TI = (new Double(System.currentTimeMillis())).doubleValue()/1000; 
+        while(true){
+            //Se verifica si existen peticiones pendientes 
+            if (this.PNS.get(1) != null){
+                //Se verifica a cual lado se movera el brazo 
+                // a traves de los metodos 
+                //Se toma el tiempo en que termina una iteracion 
+                double TFI = (new Double(System.currentTimeMillis())).doubleValue()/1000;
+                //Se actualiza el tiempo empleado en la simulacion
+                this.TTE = this.TTE + (TFI-TI);
+            } else {
+                synchronized(this){
+                    
+                    if (pausa)
+                        try {
+                            this.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(FIFO.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                        
+                }
+            }
+        }
+    }
+    
+        
+    public int proximaCercanaCreciente(){
+        List<Peticion> aux = this.PNS;
+        int distancia = 0;
+        for (int i=PI; i<4000; i++){
+            if (aux.get(1).getPista() == i){
+                distancia++;
+                break;
+            }
+            distancia++;
+        }
+        return distancia;
+    } 
+    public int proximaCercanaDecreciente(){
+        List<Peticion> aux = this.PNS;
+        int distancia = 0;
+        for (int i=PI; i>-1; i--){
+            if (aux.get(1).getPista() == i){
+                distancia++;
+                break;
+            }
+            distancia++;
+        }
+        return distancia;
+    }
+        
+    public void recorridoP(){
+        //Se toma el tiempo en que se inicia a recorrer las pistas
+        double TIR = (new Double(System.currentTimeMillis())).doubleValue()/1000;
+        
+    }
+    
+    public void recorridoN(){
+        //Se toma el tiempo en que se inicia a recorrer las pistas
+        double TIR = (new Double(System.currentTimeMillis())).doubleValue()/1000;
+        
     }
     
 }
