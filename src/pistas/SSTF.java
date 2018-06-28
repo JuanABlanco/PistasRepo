83ed8f7f5a3634c68cca3a9c8e768bdffc19aee6
ivanpbf <ivan27rlb@gmail.com>
2018-06-27 23:54:43 -0430

    SSTF Arreglado

--- src/pistas/SSTF.java
+++ src/pistas/SSTF.java
@@ -28,6 +28,16 @@ public class SSTF extends Metodos{
             if (this.PNS.get(0) != null){
                 //Se verifica a cual lado se movera el brazo 
                 // a traves de los metodos 
+                int distanciaC = 0;
+                distanciaC = proximaCercanaCreciente();
+                int distanciaD = 0;
+                distanciaD = proximaCercanaDecreciente();
+                if (distanciaC <= distanciaD){
+                    recorridoP();
+                }
+                else{
+                    recorridoN();
+                }
                 //Se toma el tiempo en que termina una iteracion 
                 double TFI = (new Double(System.currentTimeMillis())).doubleValue()/1000;
                 //Se actualiza el tiempo empleado en la simulacion
@@ -40,8 +50,7 @@ public class SSTF extends Metodos{
                             this.wait();
                     } catch (InterruptedException ex) {
                         Logger.getLogger(FIFO.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                        
+                    }             
                 }
             }
         }
@@ -73,16 +82,70 @@ public class SSTF extends Metodos{
         return distancia;
     }
         
-    public void recorridoP(){
+    public void recorridoP (){
         //Se toma el tiempo en que se inicia a recorrer las pistas
         double TIR = (new Double(System.currentTimeMillis())).doubleValue()/1000;
-        
+        for (int i=this.PI; i<4000; i++){
+            if (this.PNS.get(0).getPista() == i){
+                try {
+                    // Se toma el tiempo en el que consigue la pista adecuada
+                    double TCP = (new Double(System.currentTimeMillis())).doubleValue()/1000;
+                    //Se calcula el tiempo que le tomo encontrarla 
+                    this.TRP = TCP - TIR;
+                    //Transferencia
+                    sleep((new Double(this.PNS.get(0).getTT()).longValue())*1000);
+                    //Se acumula el tiempo de transferencia de la peticion en el tiempo de transferencia total 
+                    this.TTT = this.TTT + this.PNS.get(0).getTT();
+                    //Cambio de lista
+                    this.PS.set(1, this.PNS.remove(1)); 
+                    //Aumentar el contador de peticiones 
+                    this.NPS ++;
+                    //Aumentar el numero de pistas recorridas
+                    this.NP = this.NP + (i-this.PI);
+                    //Calcular el promedio de pistas recorridas 
+                    this.PPR = this.NP/this.NPS;
+                    //La pista actual se convierte en la pista inicial
+                    this.PI = i;
+                    //Se verifica la siguiente peticion
+                    break;   
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(FIFO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
     
-    public void recorridoN(){
+    public void recorridoN (){
         //Se toma el tiempo en que se inicia a recorrer las pistas
         double TIR = (new Double(System.currentTimeMillis())).doubleValue()/1000;
-        
+        for (int i=this.PI; i>-1; i--){
+            if (this.PNS.get(0).getPista() == i){
+                try {
+                    // Se toma el tiempo en el que consigue la pista adecuada
+                    double TCP = (new Double(System.currentTimeMillis())).doubleValue()/1000;
+                    //Se calcula el tiempo que le tomo encontrarla 
+                    this.TRP = TCP - TIR;
+                    //Transferencia
+                    sleep((new Double(this.PNS.get(0).getTT()).longValue())*1000);
+                    //Se acumula el tiempo de transferencia de la peticion en el tiempo de transferencia total 
+                    this.TTT = this.TTT + this.PNS.get(0).getTT();
+                    //Cambio de lista
+                    this.PS.set(1, this.PNS.remove(1)); 
+                    //Aumentar el contador de peticiones 
+                    this.NPS ++;
+                    //Aumentar el numero de pistas recorridas
+                    this.NP = this.NP + (this.PI-i);
+                    //Calcular el promedio de pistas recorridas 
+                    this.PPR = this.NP/this.NPS;
+                    //La pista actual se convierte en la pista inicial
+                    this.PI = i;
+                    //Se verifica la siguiente peticion
+                    break;   
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(FIFO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
     
 }
