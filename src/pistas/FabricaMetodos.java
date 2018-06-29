becf2b08f678279f15b871bc389b76a3596ee2ac
Juan Blanco <juan15597l@gmail.com>
2018-06-29 06:11:18 -0400

    Listas y vectores clonados

--- src/pistas/FabricaMetodos.java
+++ src/pistas/FabricaMetodos.java
@@ -32,16 +32,29 @@ public class FabricaMetodos {
         List<Peticion> clone3 = new ArrayList<Peticion>(l);
         List<Peticion> clone4 = new ArrayList<Peticion>(l);
         
+        int listvector [] = new int[listaVector.length];
+        int listvector1 [] = new int[listaVector.length];
+        int listvector2 [] = new int[listaVector.length];
+        int listvector3 [] = new int[listaVector.length];
+        int listvector4 [] = new int[listaVector.length];
+        
+        for (int i=0; i<listaVector.length;i++){
+            listvector[1]=listaVector[i];
+            listvector1[1]=listaVector[i];
+            listvector2[1]=listaVector[i];
+            listvector3[1]=listaVector[i];
+            listvector4[1]=listaVector[i];
+        }
         if (s.equalsIgnoreCase("FIFO")){
-            return new FIFO(clone,listaVector, PI, interfaz);
+            return new FIFO(clone,listvector, PI, interfaz);
         } else if (s.equalsIgnoreCase("SSTF")){
-            return new SSTF(clone1 ,listaVector, PI, interfaz);
+            return new SSTF(clone1 ,listvector1, PI, interfaz);
         } else if (s.equalsIgnoreCase("SCANL")){
-            return new SCANL(clone2,listaVector, PI, direccion, interfaz);
+            return new SCANL(clone2,listvector2, PI, direccion, interfaz);
         } else if (s.equalsIgnoreCase("CSCAN")){
-            return new CSCAN(clone3,listaVector, PI, direccion, interfaz);
+            return new CSCAN(clone3,listvector3, PI, direccion, interfaz);
         } else {
-            return new FSCAN(clone4,listaVector, PI, direccion, interfaz);
+            return new FSCAN(clone4,listvector4, PI, direccion, interfaz);
         }
     }
 
